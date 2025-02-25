947c3aec551275d78de680c18cdbb3ec70b3e1e4
indraprhmbd <indraprhmbd@gmail.com>
2025-02-25 10:05:20 +0700

    update file : CinemaTicket.cpp bookTickets()

--- CinemaTicket.cpp
+++ CinemaTicket.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool seats[10][9] = {false};
-int totalTickets=4,totalMovie=10,totalSession=4;
+const int totalTickets=4,totalMovie=10,totalSession=4;
 // int totalTickets=0;
 // Ticket ticket[100];
 
@@ -19,11 +19,12 @@ struct Ticket{
     int price;
 };
 
-
+string sessions[totalSession]={"10.00","13.00","15.30","18.30"};
+Movie movie[totalMovie]={"A","B","C","D","E","F","G","H","I","J"};
 
 void showTicket(int i,Ticket ticket[]){
     cout<<"====================================="<<endl;
-    cout<<"Movie \t: "<<ticket[i].movieTitle<<endl;
+    cout<<"Movie \t: "<<ticket[i].movieTitle<<endl; 
     cout<<"seat \t: "<<ticket[i].seat<<endl;
     cout<<"price \t: "<<ticket[i].price<<endl;
     // cout<<"Session : "<<ticket[i].time<<endl;
@@ -47,7 +48,15 @@ void display(){
         cout << "\n\n";
     }
 }
-void bookTickets(){
+int bookTickets(){
+    int choices[3];
+    cout<<"Choose a movie! : "<<endl;
+    for(int i=0;i<totalMovie;i++){
+        cout<<i+1<<". "<<movie[i].title<<endl;
+    }
+    cout<<"choose : ";
+    for(int i:choices) return i;
+    // return movChoosed[];
 }
 
 int searchHistory(int totalTickets,string seat,int mov,int session,Ticket ticket[],Movie movie[]){
@@ -63,15 +72,12 @@ int searchHistory(int totalTickets,string seat,int mov,int session,Ticket ticket
 string convertions(int mov,int ses,Movie movie[]){
     for(int i=0;i<totalMovie;i++){
         if(mov==i+1) movie[i].title;
-
     }
     return 0;
 }
 
 int main (){
     Ticket ticket[100];
-    Movie movie[totalMovie]={"A","B","C","D","E","F","G","H","I","J"};
-    string sessions[totalSession]={"10.00","13.00","15.30","18.30"};
     // Session time[totalSession]={"09.30","12.30","14.00","16.30"};
 
     ticket[0].seat="A1";
@@ -82,6 +88,7 @@ int main (){
     ticket[3].seat="B2";
     ticket[2].price=45000;
     ticket[3].price=35000;
+
     int i=0;
     char menu='n';
     int choice;
@@ -95,12 +102,10 @@ int main (){
         cout << "3. Exit \n";
         cout << "Choose for menu : (1/2/3) : ";
         cin >> choice;
-        
-        switch (choice)
-        {
+        switch (choice){
         case 1 :
-            display();
             bookTickets();
+            display();
             system("pause");
         break;
         case 2: 
@@ -126,7 +131,7 @@ int main (){
         break;
 
         case 3 :
-        cout << "are you sure??? (y/n) : " ; cin >>menu;
+            cout << "are you sure??? (y/n) : " ; cin >>menu;
         break;
         
         default:
